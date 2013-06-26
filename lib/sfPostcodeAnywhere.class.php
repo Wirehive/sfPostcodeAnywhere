c45da3ddfe866693ed095e8899324c85f181f1ab
Robin Corps <robin@ngse.co.uk>
2013-06-26 22:05:35 +0100

    Set API version and type. getData() method to fetch a URL and return the results

--- lib/sfPostcodeAnywhere.class.php
+++ lib/sfPostcodeAnywhere.class.php
@@ -13,10 +13,12 @@ class sfPostcodeAnywhere
   private $accountCode;
   private $licenceKey;
   private $serviceUrl;
+  private $apiVersion = 'v1.10';
+  private $apiType = 'json3';
 
 
   /**
-  * Construct the object, setting the default values.
+  * Construct the object, setting the default values
   *
   * @param string $accountCode
   * @param string $licenceKey
@@ -63,7 +65,7 @@ class sfPostcodeAnywhere
   */
   protected function prepareUrl($method, array $data = null)
   {
-    $url = $this->serviceUrl . '/' . $method . '?';
+    $url = $this->serviceUrl . '/' . $method . '/' . $this->apiVersion . '/' . $this->apiType . '.ws?';
 
     $params = array(
       'Key' => urlencode($this->licenceKey)
@@ -81,6 +83,31 @@ class sfPostcodeAnywhere
 
 
   /**
+  * Fetch the data from the given URL
+  *
+  * @param string $url
+  * @param int $timeout
+  * @return array
+  */
+  protected function getData($url, $timeout = 30)
+  {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_FAILONERROR, true);
+
+    $data = curl_exec($ch);
+
+    curl_close($ch);
+
+    // because we're always using the JSON API interface we can just return the json_decode()'d results
+    return json_decode($data);
+  }
+
+
+  /**
   * Validate a given email address using the PostcodeAnywhere API
   *
   * @param string $email
@@ -89,6 +116,7 @@ class sfPostcodeAnywhere
   */
   public function validateEmail($email)
   {
+
     return true;
   }
 }
